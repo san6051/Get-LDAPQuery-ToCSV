0862d7c3f5be0c869656986354f37944b1350a02
san6051 <75971361+san6051@users.noreply.github.com>
2025-03-24 16:56:14 +0800

    Add files via upload

---
+++ Get-LDAPQuery-ToCSV.ps1
@@ -0,0 +1,31 @@
+﻿function Get-LDAPQuery-ToCSV {
+    param (
+        [string]$query="(&(objectclass=us\65r)(!objectclass=UATtest1))",
+        [string]$exportpath
+    )
+    $searchAD = New-Object DirectoryServices.DirectorySearcher([adsi]"")
+    $SearchAD.Filter = $query
+    $SearchAD.PageSize=1000
+
+    $users=$SearchAD.FindAll()
+    if($users.Count -eq 0){break}
+    $props = [System.Collections.Generic.HashSet[String]]::new([System.StringComparer]::InvariantCultureIgnoreCase)
+    $uniqueKey=''
+    foreach($user in $users.Properties){
+    $Keys = $user.GetEnumerator() | %{$_.Key} 
+    foreach($key in $keys){$props.add($key)}
+    }
+
+    foreach($user in $users.Properties){
+    $userData = @{}
+    foreach($prop in $props){
+    $enc = [System.Text.Encoding]::UTF8
+    if($user.$prop){
+    $userData[$prop] = $user.$prop[0]}else{
+    $userData[$prop] = ""}
+    }
+    $userCustomObject = [PSCustomObject]$userData
+    $userCustomObject | Export-CSV -Path $exportpath -NoTypeInformation -Append
+    }
+
+}
